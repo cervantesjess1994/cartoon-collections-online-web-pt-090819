2b3e945c19d2c8cafbd413b8f1c09d14d40457a4
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-24 06:37:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,10 +24,12 @@ def long_planeteer_calls(array)
   else
     return false
   i = i + 1
-  end  
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+      cheese_types.include?(type)
+    end
 end
